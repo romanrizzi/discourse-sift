f516ae9199fcbecf0a0595e4b9a118f8637f1637
George Thomson <george.thomson@2hatsecurity.com>
2018-02-15 10:06:49 -0800

    allow for no topics entry in sift response

--- lib/sift.rb
+++ lib/sift.rb
@@ -7,7 +7,10 @@ class Sift
     class Risk
         attr_accessor :risk, :response
 
-       def initialize(risk:, response:, general:, bullying:, fighting:, pii:, sexting:, vulgar:, drugs:, items:, alarm:, fraud:, hate:, religious:, website:, grooming:, threats:, realname:, radicalization:, subversive:, sentiment: )
+        def initialize(risk:, response:, general:, bullying:, fighting:, pii:, sexting:, vulgar:, drugs:, items:, alarm:, fraud:, hate:, religious:, website:, grooming:, threats:, realname:, radicalization:, subversive:, sentiment: )
+            
+          Rails.logger.error("sift_debug: Risk.init enter...")            
+          
            @risk = risk,
            @response = response
            @general = general
@@ -77,6 +80,9 @@ class Sift
 
         def submit_for_classification(to_classify)
             response = post(@end_point, to_classify)
+            
+            Rails.logger.error("sift_debug: #{response.inspect}")
+            
             if response.nil? || response.status != 200
                 #if there is an error reaching Community Sift, escalate to human moderation
 
@@ -88,7 +94,11 @@ class Sift
                 response = Excon::Response.new(:body => data)
             end
 
+            
+            Rails.logger.error("sift_debug: Before validate...")
+            
             validate_classification(response)
+            
         end
 
         private
@@ -97,29 +107,35 @@ class Sift
             # TODO: Handle errors better?  Currently any issues with connection including incorrect API key leads to
             #       every post needing moderation
             hash = JSON.parse(response.body)
+            
+            Rails.logger.error("sift_debug: hash = #{hash.inspect}")
 
+            hash_topics = hash.fetch('topics', {})
+            hash_topics.default = 0
+           
+            
             result_risk = Sift::Risk.new(
                     risk:           (if hash['risk'].nil?; 0; else; hash['risk'].to_i; end;),
                     response:       !!hash['response'],
-                    general:        (if hash['topics']['0'].nil?; 0; else; hash['topics']['0'].to_i; end;),
-                    bullying:       (if hash['topics']['1'].nil?; 0; else; hash['topics']['1'].to_i; end;),
-                    fighting:       (if hash['topics']['2'].nil?; 0; else; hash['topics']['2'].to_i; end;),
-                    pii:            (if hash['topics']['3'].nil?; 0; else; hash['topics']['3'].to_i; end;),
-                    sexting:        (if hash['topics']['4'].nil?; 0; else; hash['topics']['4'].to_i; end;),
-                    vulgar:         (if hash['topics']['5'].nil?; 0; else; hash['topics']['5'].to_i; end;),
-                    drugs:          (if hash['topics']['6'].nil?; 0; else; hash['topics']['6'].to_i; end;),
-                    items:          (if hash['topics']['7'].nil?; 0; else; hash['topics']['7'].to_i; end;),
-                    alarm:          (if hash['topics']['8'].nil?; 0; else; hash['topics']['8'].to_i; end;),
-                    fraud:          (if hash['topics']['9'].nil?; 0; else; hash['topics']['9'].to_i; end;),
-                    hate:           (if hash['topics']['10'].nil?; 0; else; hash['topics']['10'].to_i; end;),
-                    religious:      (if hash['topics']['11'].nil?; 0; else; hash['topics']['11'].to_i; end;),
-                    website:        (if hash['topics']['13'].nil?; 0; else; hash['topics']['13'].to_i; end;),
-                    grooming:       (if hash['topics']['14'].nil?; 0; else; hash['topics']['14'].to_i; end;),
-                    threats:        (if hash['topics']['15'].nil?; 0; else; hash['topics']['15'].to_i; end;),
-                    realname:       (if hash['topics']['16'].nil?; 0; else; hash['topics']['16'].to_i; end;),
-                    radicalization: (if hash['topics']['17'].nil?; 0; else; hash['topics']['17'].to_i; end;),
-                    subversive:     (if hash['topics']['18'].nil?; 0; else; hash['topics']['18'].to_i; end;),
-                    sentiment:      (if hash['topics']['19'].nil?; 0; else; hash['topics']['19'].to_i; end;)
+                    general:        (if hash_topics['0'].nil?; 0; else; hash_topics['0'].to_i; end;),
+                    bullying:       (if hash_topics['1'].nil?; 0; else; hash_topics['1'].to_i; end;),
+                    fighting:       (if hash_topics['2'].nil?; 0; else; hash_topics['2'].to_i; end;),
+                    pii:            (if hash_topics['3'].nil?; 0; else; hash_topics['3'].to_i; end;),
+                    sexting:        (if hash_topics['4'].nil?; 0; else; hash_topics['4'].to_i; end;),
+                    vulgar:         (if hash_topics['5'].nil?; 0; else; hash_topics['5'].to_i; end;),
+                    drugs:          (if hash_topics['6'].nil?; 0; else; hash_topics['6'].to_i; end;),
+                    items:          (if hash_topics['7'].nil?; 0; else; hash_topics['7'].to_i; end;),
+                    alarm:          (if hash_topics['8'].nil?; 0; else; hash_topics['8'].to_i; end;),
+                    fraud:          (if hash_topics['9'].nil?; 0; else; hash_topics['9'].to_i; end;),
+                    hate:           (if hash_topics['10'].nil?; 0; else; hash_topics['10'].to_i; end;),
+                    religious:      (if hash_topics['11'].nil?; 0; else; hash_topics['11'].to_i; end;),
+                    website:        (if hash_topics['13'].nil?; 0; else; hash_topics['13'].to_i; end;),
+                    grooming:       (if hash_topics['14'].nil?; 0; else; hash_topics['14'].to_i; end;),
+                    threats:        (if hash_topics['15'].nil?; 0; else; hash_topics['15'].to_i; end;),
+                    realname:       (if hash_topics['16'].nil?; 0; else; hash_topics['16'].to_i; end;),
+                    radicalization: (if hash_topics['17'].nil?; 0; else; hash_topics['17'].to_i; end;),
+                    subversive:     (if hash_topics['18'].nil?; 0; else; hash_topics['18'].to_i; end;),
+                    sentiment:      (if hash_topics['19'].nil?; 0; else; hash_topics['19'].to_i; end;)
                 )
 
             result_risk
@@ -132,13 +148,16 @@ class Sift
             #
             request_url = "#{@api_url}/#{target}"
             request_body= {
-                'room' => "#{to_classify.topic.id}",
-                'player' => "#{to_classify.user.id}",
+                'subcategory' => "#{to_classify.topic.id}",
+                'user_id' => "#{to_classify.user.id}",
                 'text' =>  "#{to_classify.raw.strip[0..30999]}"
             }.to_json
 
             # TODO: look at using persistent connections.
             # TODO: Need to handle errors (e.g. incorrect API key)
+            
+            Rails.logger.error("sift_debug: #{request_body.inspect}")
+            
             response = begin
                  result = Excon.post(request_url,
                     body: request_body,
