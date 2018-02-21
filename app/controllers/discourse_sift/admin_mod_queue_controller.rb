1e5d4d093d4ffd644fed27024b9a7143b79f834d
George Thomson <george.thomson@2hatsecurity.com>
2018-02-20 17:30:33 -0800

    Added silence endpoint to accept duration.  Duplicates UserController.silence.  Can use i18n default reason and message

--- app/controllers/discourse_sift/admin_mod_queue_controller.rb
+++ app/controllers/discourse_sift/admin_mod_queue_controller.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 module DiscourseSift
   class AdminModQueueController < Admin::AdminController
     requires_plugin 'discourse-sift'
@@ -39,6 +41,82 @@ module DiscourseSift
       log_confirmation(post, 'dismissed')
 
       render body: nil
+    end
+
+    def silence
+      # TODO: This just copies functionality from UsersController.silence
+      # It would be nice to be able to just call that with the adjusted
+      # date
+
+      #Rails.logger.error("sift_debug: Silence Enter...")
+
+      #Rails.logger.error("sift_debug: params = #{params.inspect}")
+      
+      user = User.find_by(id: params[:user_id])
+            
+      guardian.ensure_can_silence_user! user
+
+      # If silenced_till is passed in, then use that
+      # Otherwise use duration to calculate it
+      silenced_till = params[:silenced_till]
+      if silenced_till.nil?
+        duration = params[:duration]
+        unless duration.nil?
+          # Calulate silenced_till by adding duration
+          # Duration is passed as seconds
+          temp_time = DateTime.now.to_time
+          temp_time += duration.to_i
+          silenced_till = temp_time.to_datetime
+        end
+      end
+      # Message can be sent in request, or use i18n default
+      # 
+      message = params[:message]
+      if message.nil?
+        #Rails.logger.error("sift_debug: no message")
+      
+        message = I18n.t("sift.silence.message", params)
+        #Rails.logger.error("sift_debug: message = #{message}")
+      
+      end
+
+      # Reason can be sent in request, or use i18n default
+      reason = params[:reason]
+      if reason.nil?
+        reason = I18n.t("sift.silence.reason", params)
+      end
+
+      silencer = UserSilencer.new(
+        user,
+        current_user,
+        silenced_till: silenced_till,
+        reason: reason,
+        message_body: message,
+        keep_posts: true
+      )
+      if silencer.silence && message.present?
+        Jobs.enqueue(
+          :critical_user_email,
+          type: :account_silenced,
+          user_id: user.id,
+          user_history_id: silencer.user_history.id
+        )
+      end
+
+      render_json_dump(
+        silence: {
+          silenced: true,
+          silence_reason: silencer.user_history.try(:details),
+          silenced_till: user.silenced_till,
+          suspended_at: user.silenced_at
+        }
+      )
+    end
+
+    def suspend
+      render_json_dump(
+          result: true
+      )
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ DiscourseSift::Engine.routes.draw do
       post   "confirm_failed" => "admin_mod_queue#confirm_failed"
       post   "allow"        => "admin_mod_queue#allow"
       post   "dismiss"      => "admin_mod_queue#dismiss"
+      post   "silence"      => "admin_mod_queue#silence"
+      post   "suspend"      => "admin_mod_queue#suspend"
     end
   end
 end
