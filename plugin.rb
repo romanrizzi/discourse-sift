6504e08eefe4e02a8b28adc59a1997f6173ad04a
George Thomson <george.thomson@2hatsecurity.com>
2018-02-28 11:09:33 -0800

    incrementing version to 0.1.4

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-sift
 # about: supports content classifying of posts to Community Sift
-# version: 0.1.3
+# version: 0.1.4
 # authors: Richard Kellar
 # url: https://github.com/sift/discourse-sift
 
